17f704dcdd84f78e62965d87038a6f703e5b659e
Mateusz Boczarski <matbocz@yahoo.com>
2020-01-19 18:46:25 +0100

    Create menu bar

--- main.js
+++ main.js
@@ -1,15 +1,46 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, Menu } = require('electron')
+
+const template = [{
+    label: 'Menu',
+    submenu: [
+        {
+            label: 'Przejdź do repozytorium GitHub.com',
+            click: async () => {
+                const { shell } = require('electron')
+                await shell.openExternal('https://github.com/MatBocz-PWSZ/ElectronJS-DesktopApp')
+            }
+        },
+
+        {
+            label: 'Przełącz pełny ekran',
+            role: 'togglefullscreen'
+        },
+
+        {
+            type: 'separator'
+        },
+
+        {
+            label: 'Wyjdź z aplikacji',
+            role: 'quit'
+        },
+    ]
+}]
+const menu = Menu.buildFromTemplate(template)
+
 
 let win
 
 function createWindow() {
     win = new BrowserWindow({
-        width: 800,
+        width: 850,
         height: 450,
         webPreferences: {
             nodeIntegration: true
         }
     })
+
+    Menu.setApplicationMenu(menu)
 
     win.loadFile('index.html')
 
